07e7fad0a447111edc60afc4748d0483c4589624
20323702-beep <20323702@clases.edu.sv>
2026-06-03 19:13:51 -0600

    Create conversionales.2.py

---
+++ practica 6/conversionales.2.py
@@ -0,0 +1,11 @@
+print("Escribe tu nombre")
+nombre = input()
+
+if nombre == "Rodrigo":
+    print("Saludos Rodrigo")
+elif nombre == "Alejandro":
+    print("Hola Alejandro")
+elif nombre == "Carlos":
+    print("Qué tal Carlos")
+else:
+    print("Hola desconocido")
