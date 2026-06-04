5caf389e368e295b06d3fcc50bcd2289c16b6257
20323702-beep <20323702@clases.edu.sv>
2026-06-03 19:01:52 -0600

    Create ciclos.py

---
+++ practica 4/ciclos.py
@@ -0,0 +1,11 @@
+# Inicialización del contador para el ciclo
+número = 0
+
+# Ciclo condicional while
+while número < 5:
+    print("Hola", número)
+    número = número + 1
+
+# Ciclo definido empleando for y range
+for i in range(1, 11):
+    print(i)
