fce0858fddd1d2eece43c2ac2076265c678b181b
20323702-beep <20323702@clases.edu.sv>
2026-06-03 19:21:35 -0600

    Create tienda 2.py

---
+++ practica 11/tienda 2.py
@@ -0,0 +1,17 @@
+#--------------------------part 2---------------------------
+print("*********************************")
+print("          Bienvenido a           ")
+print("       La tienda de mascotas     ")
+print("*********************************")
+
+num_perros = 10
+num_gatos = 8
+num_pájaros = 25
+
+print("Actualmente contamos con")
+print("Perros:", num_perros, "Gatos:", num_gatos, "Pájaros:", num_pájaros)
+
+# Declaración de variable para almacenar la suma total
+animales_totals = num_perros + num_gatos + num_pájaros
+
+print("En total tenemos", animales_totals, "animales")
