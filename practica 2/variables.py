2c10bb8ffee60252692dfc1ba93cd0f045bb4dc8
20323702-beep <20323702@clases.edu.sv>
2026-06-03 18:55:55 -0600

    Create  variables.py

---
+++ practica 2/variables.py
@@ -0,0 +1,23 @@
+# Soy un comentario.
+nombre = "Rodrigo"  # declarando la variable Nombre.
+apellido = "Montemayor"
+
+print(nombre, apellido)
+
+# Las variables pueden cambiar su valor
+nombre = "Alejandro"
+print(nombre, apellido)
+
+# Tipos de datos numéricos y expresiones
+edad = 20
+print(nombre, apellido, edad)
+
+# Modificar el valor de una variable usando una expresión
+edad = edad + 15  # Suma (Da 35)
+edad = edad - 15  # Resta (Regresa a 20)
+edad = edad * 2   # Multiplicación (Da 40)
+edad = edad / 5   # División (Convierte a float: 8.0)
+
+# El video muestra también ejemplos complejos y el uso de type()
+print(print(type(nombre))) # Muestra <class 'str'>
+print(print(type(edad)))   # Muestra <class 'float'> o <class 'int'> según la línea anterior
